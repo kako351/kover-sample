c8f17afc07efd57b0274d77f0ec3a9b158e60841
kako351 <h3.birth@gmail.com>
2024-07-10 00:36:19 +0900

    install kover

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
+    id("org.jetbrains.kotlinx.kover") version "0.8.2"
 }
